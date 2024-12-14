f8fef7880ed3b93e322c9d1812e7b0012afd2d41
Milad Sadeghi <miladsadeghi2323@gmail.com>
2024-12-14 23:28:55 +0330

    feat: Add Jenkins to the Installation

---
+++ src/pages/installation/jenkins/data/select-options.ts
@@ -0,0 +1,25 @@
+export const osOptions = [
+  {
+    label: 'Ubuntu',
+    value: 'Ubuntu',
+  },
+  {
+    label: 'Fedora',
+    value: 'Fedora',
+  },
+  {
+    label: 'RHEL',
+    value: 'RHEL',
+  },
+];
+
+export const environmentOptions = [
+  {
+    label: 'Linux',
+    value: 'Linux',
+  },
+  {
+    label: 'Docker',
+    value: 'Docker',
+  },
+];
