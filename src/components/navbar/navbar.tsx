4bbccacec8b4db26df5d5f5185c28a4f0dceafd0
Milad Sadeghi <miladsadeghi2323@gmail.com>
2025-01-03 16:38:50 +0330

    fix: Resolve submenu issues in the navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,12 @@
-import { createRef, FC, RefObject, useEffect, useRef, useState } from 'react';
+import {
+  createRef,
+  FC,
+  MouseEvent,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { NavLink, useLocation } from 'react-router';
 import { User } from './components/user';
 import { cn } from '../../lib/utils';
@@ -153,6 +161,11 @@ const navbar = [
 const Navbar: FC = () => {
   const [menu, setMenu] = useState<'Chats' | 'Templates' | 'Installation'>();
   const [position, setPosition] = useState({ left: 0 });
+  const [activeSubSubMenu, setActiveSubSubMenu] = useState();
+  const [subSubMenuPosition, setSubSubMenuPosition] = useState({
+    top: 0,
+    left: 0,
+  });
 
   const location = useLocation();
 
@@ -182,7 +195,8 @@ const Navbar: FC = () => {
   }, [menu]);
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    const handleClickOutside = (event: any) => {
+      setActiveSubSubMenu(undefined);
       if (navRef.current && !navRef.current.contains(event.target as Node)) {
         setMenu(undefined);
       }
@@ -196,6 +210,7 @@ const Navbar: FC = () => {
   }, []);
 
   useEffect(() => {
+    setActiveSubSubMenu(undefined);
     const findActiveMenu = () => {
       let active = { parentTitle: '', subTitle: '', subSubTitle: '' };
 
@@ -235,108 +250,118 @@ const Navbar: FC = () => {
     }
   };
 
-  const handleHoverMenuItem = (item: string) => {
-    setActiveMenu((prev) => ({
-      ...prev,
-      subSubTitle: item,
-    }));
+  const handleHoverMenuItem = (subItem: any, e?: MouseEvent) => {
+    setActiveSubSubMenu(subItem);
+    if (e) {
+      const rect = (e.currentTarget as HTMLElement).getBoundingClientRect();
+      setSubSubMenuPosition({
+        top: rect.top,
+        left: rect.left + rect.width + 10,
+      });
+    }
   };
 
   return (
-    <div className="relative mt-4 flex w-full justify-center">
-      <nav
-        ref={navRef}
-        className="isolate z-10 flex w-full items-center justify-between rounded-lg border border-gray-800 bg-gray-500/10 bg-clip-padding pr-4 backdrop-blur backdrop-contrast-100 backdrop-saturate-100 backdrop-filter"
-      >
-        <div className="flex items-center">
-          {navbar.map((item, index) => (
-            <button
-              ref={buttonRefs.current[index]}
-              onClick={() => handleButtonClick(item.title)}
-              key={item.title}
-              className={cn(
-                'w-full cursor-pointer whitespace-nowrap px-4 py-3 text-center',
-                {
-                  'text-orchid-medium': activeMenu.parentTitle === item.title,
-                },
-              )}
-            >
-              {item.title}
-            </button>
-          ))}
-        </div>
-        <User />
-      </nav>
-      {menu && (
-        <div
-          ref={menuRef}
-          className="absolute top-14 z-[60] w-fit rounded-md transition-all"
-          style={{
-            left: position.left,
-          }}
+    <>
+      <div className="relative mt-4 flex w-full justify-center">
+        <nav
+          ref={navRef}
+          className="isolate z-10 flex w-full items-center justify-between rounded-lg border border-gray-800 bg-gray-500/10 bg-clip-padding pr-4 backdrop-blur backdrop-contrast-100 backdrop-saturate-100 backdrop-filter"
         >
-          <div className="flex h-fit w-full flex-col gap-2 rounded-lg border border-gray-800 bg-gray-400/10 bg-clip-padding backdrop-blur-lg backdrop-contrast-100 backdrop-saturate-100 backdrop-filter">
-            {navbar.map(
-              (item) =>
-                item.subMenu &&
-                item.title === menu &&
-                item.subMenu.map((subItem) =>
-                  'subMenu' in subItem ? (
-                    <button
-                      onMouseEnter={() => handleHoverMenuItem(subItem.title)}
-                      key={subItem.title}
-                      className={cn(
-                        'relative w-full px-4 py-3 text-center text-white',
-                        {
-                          'text-orchid-medium':
-                            activeMenu.subTitle === subItem.title,
-                        },
-                      )}
-                    >
-                      {subItem.title}
-                      <div
-                        onMouseLeave={() => handleHoverMenuItem('')}
+          <div className="flex items-center">
+            {navbar.map((item, index) => (
+              <button
+                ref={buttonRefs.current[index]}
+                onClick={() => handleButtonClick(item.title)}
+                key={item.title}
+                className={cn(
+                  'w-full cursor-pointer whitespace-nowrap px-4 py-3 text-center',
+                  {
+                    'text-orchid-medium': activeMenu.parentTitle === item.title,
+                  },
+                )}
+              >
+                {item.title}
+              </button>
+            ))}
+          </div>
+          <User />
+        </nav>
+        {menu && (
+          <div
+            ref={menuRef}
+            className="absolute top-14 z-[60] w-fit rounded-md border border-gray-800 bg-gray-400/10 bg-clip-padding backdrop-blur-lg backdrop-contrast-100 backdrop-saturate-100 backdrop-filter transition-all"
+            style={{
+              left: position.left,
+            }}
+          >
+            <div className="flex h-fit w-full flex-col gap-2 rounded-lg">
+              {navbar.map(
+                (item) =>
+                  item.subMenu &&
+                  item.title === menu &&
+                  item.subMenu.map((subItem) =>
+                    'subMenu' in subItem ? (
+                      <button
+                        onMouseEnter={(e) => handleHoverMenuItem(subItem, e)}
+                        key={subItem.title}
                         className={cn(
-                          'absolute left-full top-0 ml-2 hidden h-fit w-full flex-col gap-2 overflow-y-hidden rounded-lg border border-gray-800 bg-slate-900',
+                          'relative w-full px-4 py-3 text-center text-white',
                           {
-                            flex: activeMenu.subSubTitle === subItem.title,
+                            'text-orchid-medium':
+                              activeMenu.subTitle === subItem.title,
                           },
                         )}
                       >
-                        {subItem.subMenu.map((subSubItem) => (
-                          <NavLink
-                            to={(subSubItem as any).link}
-                            key={subSubItem.title}
-                            className={({ isActive }) =>
-                              cn('w-full px-8 py-3 text-center text-white', {
-                                'text-orchid-medium': isActive,
-                              })
-                            }
-                          >
-                            {subSubItem.title}
-                          </NavLink>
-                        ))}
-                      </div>
-                    </button>
-                  ) : (
-                    <NavLink
-                      to={(subItem as any).link}
-                      key={subItem.title}
-                      className={({ isActive }) =>
-                        cn('w-full px-8 py-3 text-center text-white', {
-                          'text-orchid-medium': isActive,
-                        })
-                      }
-                    >
-                      {subItem.title}
-                    </NavLink>
+                        {subItem.title}
+                      </button>
+                    ) : (
+                      <NavLink
+                        to={(subItem as any).link}
+                        key={subItem.title}
+                        className={({ isActive }) =>
+                          cn('w-full px-8 py-3 text-center text-white', {
+                            'text-orchid-medium': isActive,
+                          })
+                        }
+                      >
+                        {subItem.title}
+                      </NavLink>
+                    ),
                   ),
-                ),
-            )}
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+      <div
+        onMouseLeave={() => handleHoverMenuItem(undefined)}
+        style={{
+          left: subSubMenuPosition.left,
+          top: subSubMenuPosition.top,
+        }}
+        className={cn(
+          'absolute left-full top-0 z-50 ml-2 hidden h-fit w-96 flex-col gap-2 overflow-y-hidden rounded-lg border border-gray-800 bg-gray-400/10 bg-clip-padding backdrop-blur-lg backdrop-contrast-100 backdrop-saturate-100 backdrop-filter',
+          {
+            flex: activeSubSubMenu,
+          },
+        )}
+      >
+        {(activeSubSubMenu as any)?.subMenu.map((subSubItem: any) => (
+          <NavLink
+            to={(subSubItem as any).link}
+            key={subSubItem.title}
+            className={({ isActive }) =>
+              cn('w-full px-8 py-3 text-center text-white', {
+                'text-orchid-medium': isActive,
+              })
+            }
+          >
+            {subSubItem.title}
+          </NavLink>
+        ))}
+      </div>
+    </>
   );
 };
 
